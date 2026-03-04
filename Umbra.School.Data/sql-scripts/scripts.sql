2789e89c391813ff8f2bf900cd1c457008bc8b4d
shaozelian <zsh.rc@outlook.com>
2026-03-04 11:47:53 +0800

    add more options for test words numbers

--- Umbra.School.Data/sql-scripts/scripts.sql
+++ Umbra.School.Data/sql-scripts/scripts.sql
@@ -6,3 +6,7 @@ insert into UserRoles (userId, roleid) values (
 	(select top 1 id from users where username = 'shaozelian@live.com'),
 	(select top 1 id from roles where normalizedname = 'ADMINISTRATORS')
 )
+
+update users set firstname = N'泽连', lastname = N'邵' where username = 'shaozelian@live.com'
+update users set firstname = N'家誉', lastname = N'邵' where username = 'jiayu.sh.shao@outlook.com'
+update users set firstname = N'家然', lastname = N'邵' where username = 'jiaran.shao@outlook.com'
